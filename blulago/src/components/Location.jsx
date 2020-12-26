9ed7616d8dc23d3360c77c7c871d403c262a0e10
Jamie Tardi <jamieTardi>
2020-12-26 13:00:24 +0000

    added location

--- blulago/src/components/Location.jsx
+++ blulago/src/components/Location.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
+import locationPic from '../assets/location.png'
 
 
 const Location = () => {
     return (
         <FlexContainer>
+            <FlexPic>
             <LocationPic>
-
+                <h4>Getting Here</h4>
+                <p>The Best way to get to the lake is to drive from Calais (ferry or eurotunnel). Once you get off the ferry head to A26, when you get on the A26 continue all the way to Reims, this is a good 2 hours 30 min and the bulk of the drive. When you get to reims you will see signs for the A4 signposted Troyes. Take exit 21 for Vallée de l'Aube, then follow the D441 until the D75 then the D180. This will eventually lead you to Rue des Carriéres. 
+Total Drive time from Calais is about 3 hours 30 mins at a leisurely pace in a van</p>
             </LocationPic>
             <LocationPic>
 
             </LocationPic>
-            
+            </FlexPic>
             <LocationContainer>
-
+            <img src={locationPic} alt=""/>
 
             </LocationContainer>
             
@@ -32,15 +36,37 @@ background-color: #e3dfdb;
 width: 70vw;
 height: 60vh;
 margin: 1rem 1rem;
+img{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
 `
 
 const LocationPic = styled.div`
-border: 2px solid black;
+border: none;
 width: 25vw;
 height: 29vh;
 margin-top: 1rem;
 display: flex;
+background-color: #e3dfdb;
+display: flex;
 flex-direction: column;
-`
+text-align: center;
+p{
+    font-size: 0.8rem;
+    
+}
+h4{
+    color: #595959;
+font-family: 'Lobster Two', cursive;
+margin-bottom: 1rem;
+}
 
+
+`
+const FlexPic = styled.div`
+
+margin-left: 1rem
+`
 export default Location
