e7e059dd4efa754288fdf49fa6e61601c16ea7f3
jamieTardi <jamie.tardi@gmail.com>
2020-12-27 14:48:06 +0000

    shop adjustments

--- blulago/src/pages/ShopHome.jsx
+++ blulago/src/pages/ShopHome.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { v4 as uuidv4 } from 'uuid';
 import styled from 'styled-components'
+import Footer from '../components/Footer'
 
 
 const ShopHome = () => {
@@ -22,24 +23,65 @@ const shopItems = [{
     avalible: 0,
     id: uuidv4()
 },
+{
+    name: 'Lake Exclusive',
+    arrival: '09/08/2020',
+    departure: '16/08/2020',
+    picture:  'https://upload.wikimedia.org/wikipedia/commons/d/d1/Mount_Everest_as_seen_from_Drukair2_PLW_edit.jpg',
+    price: '£800',
+    avalible: 0,
+    id: uuidv4()
+},
+{
+    name: 'Lake Exclusive',
+    arrival: '09/08/2020',
+    departure: '16/08/2020',
+    picture:  'https://upload.wikimedia.org/wikipedia/commons/d/d1/Mount_Everest_as_seen_from_Drukair2_PLW_edit.jpg',
+    price: '£800',
+    avalible: 1,
+    id: uuidv4()
+},
+{
+    name: 'Lake Exclusive',
+    arrival: '09/08/2020',
+    departure: '16/08/2020',
+    picture:  'https://upload.wikimedia.org/wikipedia/commons/d/d1/Mount_Everest_as_seen_from_Drukair2_PLW_edit.jpg',
+    price: '£800',
+    avalible: 0,
+    id: uuidv4()
+},
+{
+    name: 'Lake Exclusive',
+    arrival: '09/08/2020',
+    departure: '16/08/2020',
+    picture:  'https://upload.wikimedia.org/wikipedia/commons/d/d1/Mount_Everest_as_seen_from_Drukair2_PLW_edit.jpg',
+    price: '£800',
+    avalible: 1,
+    id: uuidv4()
+},
 ]
 
     return (
+        <>
         <ShopContainer>
         <CardContainer>
             {shopItems.map((holiday) => (
                 <Card key={holiday.id}>
-                    <img src={holiday.picture} alt="fishing pic"/>
                     <h4>{holiday.name}</h4>
-                    <p classname="dropDwn">Arrival Date <br/><span>{holiday.arrival}</span></p>
-                    <p classname="dropDwn">Departure Date <br/><span>{holiday.departure}</span></p>
-                    <h4 classname="dropDwn">Deposit Price <br/><span>{holiday.price}</span></h4>
-                    <p classname="dropDwn" style={holiday.avalible > 0 ? {color: 'green'} : {color: 'red'}}>Avalible: <br/><span>{holiday.avalible ? 'Yes' : 'No'}</span></p>
-                    <button >Add to Cart</button>
+                    <img src={holiday.picture} alt="fishing pic"/>
+                    <p className="dropDwn arrivalDepart">Arrival Date <br/><span>{holiday.arrival}</span></p>
+                    <p className="dropDwn arrivalDepart">Departure Date <br/><span>{holiday.departure}</span></p>
+                    <h4 className="dropDwn">Deposit Price <br/><span>{holiday.price}</span></h4>
+                    <p className="dropDwn" style={holiday.avalible > 0 ? {color: 'green'} : {color: 'red'}}>Avalible: <br/><span>{holiday.avalible ? 'Yes' : 'No'}</span></p>
+                    <button style={holiday.avalible > 0 ? {opacity: 1} : {opacity: 0, pointerEvents: 'none'}}>Add to Cart</button>
                 </Card>
     ))}
     </CardContainer>
+    <Footer/>
         </ShopContainer>
+        
+        </>
+        
     )
 }
 
@@ -47,25 +89,37 @@ const shopItems = [{
 
 const CardContainer = styled.div`
 display: flex;
+flex-wrap: wrap;
 gap: 1rem;
-position: relative
+position: relative;
+padding: 0rem 4rem;
+margin-bottom: 2rem;
+.arrivalDepart{
+    font-size: 0.7rem;
+}
 `
 
 const ShopContainer = styled.div`
 width: 100vw;
 height: 100vh;
-padding: 2rem 5rem
+padding: 2rem 0rem
 `
 
 const Card = styled.div`
 width: 25vw;
 height: 55vh;
+flex: 1 0 30%;
 border: none;
 display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center; 
 box-shadow: 5px 5px 15px 5px #000000;
+background: #E3DFDB;
+@media(max-width: 600px){
+    flex: 1 2 50%;
+};
+
 
 .dropDwn{
     text-align: center;
