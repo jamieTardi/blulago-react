a91c873d0d748c90b18c8c6a1fd8960421c3dee1
Jamie Tardi <jamieTardi>
2020-12-28 10:39:53 +0000

    added e commerce api

--- blulago/src/App.js
+++ blulago/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import GlobalStyles from './GlobalStyles'
 import NavBar from './components/NavBar'
 import Home from './pages/Home'
@@ -6,15 +6,26 @@ import {Switch, Route, useLocation } from 'react-router-dom'
 import ShopHome from './pages/ShopHome'
 import Cart from './components/Cart'
 import Gallery from './pages/Gallery'
+import {commerce} from './lib/commerce'
 
 function App() {
   const [cart, setCart] = useState([])
+  const [holidays, setHolidays] = useState([])
   const location = useLocation();
+
+  const fetchHolidays = async() => {
+    const {data} = await commerce
+        .products
+        .list()
+        setHolidays(data)
+}
 
 
   const handleAddToCart = (productId, quantity, arrival, departure) => {
     setCart([...cart, {name: productId, quantity, arrival, departure}])
   }
+
+  useEffect(() => {fetchHolidays()},[])
   return (
     <div className="App">
       <GlobalStyles/>
@@ -24,13 +35,13 @@ function App() {
       <Home/>
       </Route>
       <Route exact path="/shopHome">
-        <ShopHome cart={cart} handleAddToCart={handleAddToCart}/>
+        <ShopHome cart={cart} handleAddToCart={handleAddToCart} holidays={holidays}/>
       </Route>
       <Route exact path="/gallery">
         <Gallery />
       </Route>
       <Route exact path="/cart">
-        <Cart  />
+        <Cart  cart={cart}/>
       </Route>
     
       </Switch>
--- blulago/src/components/Cart.jsx
+++ blulago/src/components/Cart.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
+import { v4 as uuidv4 } from 'uuid';
 
-const Cart = () => {
+
+const Cart = ({cart}) => {
+    console.log(cart)
     return (
-        <div>
-            <p>Cart</p>
-        </div>
+        <CartContainer>
+            <h2>Your Cart</h2>
+            {cart.map((holiday) => (
+                <div key={uuidv4()}>
+                
+                <p>{holiday.name}</p>
+                </div>
+                
+                ))}
+                
+        </CartContainer>
     )
 }
 
+const CartContainer = styled.div`
+width: 100vw;
+height: 70vh;
+border: 2px solid black;
+h2{
+    color: black;
+    text-align: center;
+}
+
+`
 export default Cart
