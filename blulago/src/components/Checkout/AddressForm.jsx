0c8ebeb9fc9cbfc3cb5759a9ebb3911761004fea
Jamie Tardi <jamieTardi>
2020-12-29 12:56:20 +0000

    added address form

---
+++ blulago/src/components/Checkout/AddressForm.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect} from 'react'
+import { InputLabel, Select, MenuItem, Button, Grid, Typography} from '@material-ui/core'
+import { useForm, FormProvider} from 'react-hook-form'
+import FormInput from './FormInput'
+import {commerce} from '../../lib/commerce'
+import {Link} from 'react-router-dom'
+
+const AddressForm = ({next, checkoutToken}) => {
+
+  const methods = useForm()
+  return (
+    <div>
+      <Typography variant="h6" gutterBottom>Shipping Options</Typography>
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit((data) => next({...data,},console.log(data)))}>
+        <Grid container spacing={3}>
+          <FormInput name="firstName" label='First Name'/>
+          <FormInput name="lastName" label='Last Name'/>
+          <FormInput name="email" label='Email'/>
+          <FormInput name="address1" label='Address'/>
+          <FormInput name="city" label='City/Town'/>
+          <FormInput name="zip" label='Postcode'/>
+          <FormInput name="phone" label='Phone Number'/>
+
+
+        </Grid>
+        <br/>
+                   <div style={{display: 'flex', justifyContent:'space-between'}}>
+                        <Button component={Link} to="/cart" variant='outlined'>Back to Cart</Button>
+                        <Button variant='contained' type="submit" color="primary">Next</Button>
+
+                   </div>
+        </form>
+
+      </FormProvider>
+    </div>
+  )
+}
+
+export default AddressForm
+
--- blulago/src/components/Checkout/Checkout.jsx
+++ blulago/src/components/Checkout/Checkout.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import CheckoutForm from './CheckoutForm'
+import AddressForm from './AddressForm'
 import PaymentForm from './PaymentForm'
 import {Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button} from '@material-ui/core'
 import useStyles from './styles'
@@ -11,16 +11,23 @@ const steps = ['Shipping Address', 'Payment Details']
 const Checkout = ({cart}) => {
     const [activeStep, setActiveStep] = useState(0)
     const [checkoutToken, setCheckoutToken] = useState(null)
+    const [shippingData, setShippingData] = useState({})
 
     const classes = useStyles()
-    const Form = () => activeStep === 0 ? <CheckoutForm /> : <PaymentForm/>
-    console.log(cart)
+    const nextStep = () => setActiveStep((prev) => ++prev )
+    const backStep = () => setActiveStep((prev) => --prev)
+
+    const next = (data) => {
+        setShippingData(data)
+        nextStep()
+    }
+    const Form = () => activeStep === 0 ? <AddressForm checkoutToken={checkoutToken} next={next} /> : <PaymentForm/>
     useEffect(() => {
         const generateToken = async () => {
             try{
              const token = await commerce.checkout.generateToken(cart.id, {type: 'cart'})
              
-             console.log(token)
+            
              setCheckoutToken(token)
              
             }
